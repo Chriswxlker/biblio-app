b71c96d34a8561816474888d5f31ecd8a9598a45
Chriswxlker <cristiandavidredondo7@gmail.com>
2025-05-08 23:20:17 -0500

    Intgrando tailwind y avanzando en el index.ejs

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(
   session({
@@ -35,6 +35,8 @@ app.use(
 );
 
 app.use(flash());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -53,5 +55,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(express.static('public'));
 
 module.exports = app;
